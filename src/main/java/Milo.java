07bebd46f9efd0f4d732218954c0ffcae4f332b8
Javier-Lua <javier_lua@outlook.com>
2025-08-18 16:36:46 +0800

    Renamed chatbot, greets user and exits.
    Removed all traces of old name and replaced with "Milo".
    Initial skeletal version implemented via System.out.println.

---
+++ src/main/java/Milo.java
@@ -0,0 +1,10 @@
+public class Milo {
+    public static void main(String[] args) {
+        System.out.println("____________________________________________________________\n" +
+                " Hello! I'm Milo!\n" +
+                " What can I do for you?\n" +
+                "____________________________________________________________\n" +
+                " Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________\n");
+    }
+}
