0192c049e697175f420db0226d88ffd005d49876
Javier-Lua <javier_lua@outlook.com>
2025-08-18 17:25:00 +0800

    Store & display text
    Chatbot now stores input given by user
    and displays it back to the user
    when "list" is entered.

--- src/main/java/Milo.java
+++ src/main/java/Milo.java
@@ -2,17 +2,33 @@ import java.util.Scanner;
 
 public class Milo {
     public static void main(String[] args) {
+        // Assuming no more than 100 tasks
+        String[] tasks = new String[100];
         Scanner sc = new Scanner(System.in);
         System.out.println("____________________________________________________________\n" +
                 " Hello! I'm Milo!\n" +
                 " What can I do for you?\n" +
                 "____________________________________________________________\n");
-        String command = sc.next();
-        while(!command.equals("bye")) {
-            System.out.println("____________________________________________________________\n" +
-                    command + "\n" +
-                    "____________________________________________________________\n");
-            command = sc.next();
+        String task = sc.nextLine();
+        int itemInd = 0;
+        while(!task.equals("bye")) {
+            if (task.equals("list")) {
+                String[] list = new String[itemInd]; // copy over tasks array into smaller array
+                for (int i = 0; i < itemInd; i++) {
+                    list[i] = tasks[i];
+                }
+                System.out.println("____________________________________________________________\n" +
+                        String.join("\n", list) + "\n" + // faster than += string concat
+                        "____________________________________________________________\n");
+            } else {
+                int num = itemInd + 1;
+                tasks[itemInd] = num + ". " + task; // number task once added
+                itemInd += 1;
+                System.out.println("____________________________________________________________\n" +
+                        "added: " + task + "\n" +
+                        "____________________________________________________________\n");
+            }
+            task = sc.nextLine();
         }
         System.out.println("____________________________________________________________\n" +
                 " Bye. Hope to see you again soon!\n" +
