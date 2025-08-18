12257a232c67532cf40b126c9188b9082cd8e57a
Javier-Lua <javier_lua@outlook.com>
2025-08-18 16:54:21 +0800

    Chatbot echoes commands.
    Milo now echoes commands entered by the user, and
    exits when the user says "bye".

--- src/main/java/Milo.java
+++ src/main/java/Milo.java
@@ -1,9 +1,20 @@
+import java.util.Scanner;
+
 public class Milo {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("____________________________________________________________\n" +
                 " Hello! I'm Milo!\n" +
                 " What can I do for you?\n" +
-                "____________________________________________________________\n" +
+                "____________________________________________________________\n");
+        String command = sc.next();
+        while(!command.equals("bye")) {
+            System.out.println("____________________________________________________________\n" +
+                    command + "\n" +
+                    "____________________________________________________________\n");
+            command = sc.next();
+        }
+        System.out.println("____________________________________________________________\n" +
                 " Bye. Hope to see you again soon!\n" +
                 "____________________________________________________________\n");
     }
